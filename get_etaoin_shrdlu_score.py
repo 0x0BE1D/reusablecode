9316cc46c0a60e1ef77a0c604097f730bc56d5e0
obeid10 <obeid2@gmail.com>
2023-04-11 15:16:50 +1000

    Update get_etaoin_shrdlu_score.py
    added 'import string'

--- get_etaoin_shrdlu_score.py
+++ get_etaoin_shrdlu_score.py
@@ -1,3 +1,4 @@
+import string
 def get_etaoin_shrdlu_score(text):
     # Define the expected frequency distribution of letters in English text
     freq_dist = {'e': 12.7, 't': 9.1, 'a': 8.2, 'o': 7.5, 'i': 7.0, 'n': 6.7, 's': 6.3, 'h': 6.1, 'r': 6.0, 'd': 4.3, 'l': 4.0, 'u': 2.8, 'c': 2.8, 'm': 2.4, 'w': 2.4, 'f': 2.2, 'g': 2.0, 'y': 2.0, 'p': 1.9, 'b': 1.5, 'v': 1.0, 'k': 0.8, 'j': 0.2, 'x': 0.2, 'q': 0.1, 'z': 0.1}
